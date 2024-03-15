cdc6e558cf93eae0b563bdf6c67055803401d306
Andrea <dea0312@gmail.com>
2024-03-14 21:22:08 -0300

    feature: Respostas da aula 2 - variáveis.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@
 const idade = 25;
 //console.log(idade); 
 
-
-
+// ******Resposta: TypeError - Atribuição a uma constante, variável já declarada. (não pode ser reatribuida.)
 
 
 ////////////////////////////////////////////////////////////////////////
@@ -20,7 +19,8 @@ const idade = 25;
 }
 // console.log(x);
 
-
+// ********Resposta: Vai aparecer a variavel de dentro do esocpo. Já a variavel de fora aparecerá como "is not defined", 
+// ****ou seja, a variável x não foi definido
 
 ///////////////////////////////////////////////////////////////////////
 
@@ -34,35 +34,40 @@ const idade = 25;
 // console.log(x);
 // console.log(y);
 
+// *****Resposta: No console x nãi aparece o valor atribuido aparece como x não definido, e o console y aparece o valor que foi atribuido
+
 ///////////////////////////////////////////////////////////////////////
 
 //O que acontece nessa declaração, explique sobre o Hoisting
 
 //console.log(a); 
 var a = 5;
-console.log(b);
-//let b = 10;
+//console.log(b);
+let b = 10;
 
+// *****Resposta: A variável a aparece como undefined; já a variável b diz que não é possivel acessar antes da inicialização.
+// ***** No Hoisting a variável criada é elevada para o escopo, mas sem o valor. 
 
 /////////////////////////////////////////////////////////////////////////////////
 
 //No código abaixo apresenta um erro, qual?
 
-var x = 5;
-var x = 10;
-// console.log(x); 
+// var x = 5;
+// var x = 10;
+//  console.log(x); 
 
-let y = 15;
-let y = 20; 
+// let y = 15;
+// let y = 20; 
 // console.log(y); 
 
+// *******Resposta: Há erro de sintaxe, pois a variável y já foi declarada.
 
 //// Escreva um programa que print a primeira letra dessa variavel 
 let nomeCompleto = 'eduardo'
 
 //sua lógica para pegar a primeira letra 
-/// const letraInicial = ...
-//console.log(letraInicial)
+const letraInicial = nomeCompleto[0];
+console.log(letraInicial)
 
 
 
